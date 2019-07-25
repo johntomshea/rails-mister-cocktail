e1cacd2526a70d08edbc58660ae02ba9272a17c3
John Shea <johntomshea@gmail.com>
2019-07-25 14:44:09 +0800

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Eradication'
+Ingredient.destroy_all
+puts 'Seeding new life'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |hash|
+  ingredient = {}
+  ingredient[:name] = hash["strIngredient1"]
+  Ingredient.create(ingredient)
+end
+puts 'Mischief managed'
+puts Ingredient.all
